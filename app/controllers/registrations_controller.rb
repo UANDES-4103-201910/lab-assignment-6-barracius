9441904c125d19b09e2f621c8e9900f2a1c0650a
barracius <rjgonzalez6@gmail.com>
2019-04-17 11:24:18 -0400

    Sign in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,5 +5,24 @@ class RegistrationsController < ApplicationController
 
 	def create
 	    #complete this method
+	    if params[:registrations][:name].blank? or params[:registrations][:last_name].blank? or params[:registrations][:password].blank? or params[:registrations][:phone].blank? or params[:registrations][:email].blank?
+	    	flash[:notice] = "All fields must be filled"
+	    	render 'new'
+		else
+			@user = User.new
+			@user.name = params[:registrations][:name]
+			@user.last_name = params[:registrations][:last_name]
+			@user.password = params[:registrations][:password]
+			@user.email = params[:registrations][:email]
+			@user.phone = params[:registrations][:phone]
+			if @user.save!
+				flash[:notice] = "User created"
+				redirect_to root_url
+			else
+				flash[:notice] = "Validation error"
+				render 'new'
+			end
+		end
+
 	end
 end
